6870cf981762013fec8bf5bd274f91edc32008f6
sukrutd <sukrut.dd@gmail.com>
2020-12-01 16:14:42 +0530

    Updated container

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import MarketingApp from './components/MarketingApp';
 
 export default () => {
-  return (
-    <div>
-      <h1>Hi there!</h1>
-      <hr />
-      <MarketingApp />
-    </div>
-  );
+	return (
+		<div>
+			<h1>Welcome!</h1>
+			<p>This is a container</p>
+			<hr />
+			<MarketingApp />
+		</div>
+	);
 };
