dccc19bed663d508d092101cd9e0e56f8d08cae0
sukrutd <sukrut.dd@gmail.com>
2020-12-01 19:23:16 +0530

    CSS conflict resolution with namespacing

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,21 +1,20 @@
 import React from 'react';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
-import { StylesProvider } from '@material-ui/core/styles';
-
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
 import Landing from './components/Landing';
 import Pricing from './components/Pricing';
 
+const generateClassName = createGenerateClassName({ productionPrefix: 'ma' });
+
 export default () => {
-  return (
-    <div>
-      <StylesProvider>
-        <BrowserRouter>
-          <Switch>
-            <Route exact path="/pricing" component={Pricing} />
-            <Route path="/" component={Landing} />
-          </Switch>
-        </BrowserRouter>
-      </StylesProvider>
-    </div>
-  );
+	return (
+		<BrowserRouter>
+			<Switch>
+				<StylesProvider generateClassName={generateClassName}>
+					<Route exact path='/pricing' component={Pricing} />
+					<Route path='/' component={Landing} />
+				</StylesProvider>
+			</Switch>
+		</BrowserRouter>
+	);
 };
